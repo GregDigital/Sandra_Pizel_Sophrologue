e3c206897c8d307e42bffdddd159dca0f7e05aae
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-05-03 00:42:44 +0200

    homePage

--- nav.js
+++ nav.js
@@ -3,6 +3,15 @@ var burgerMenu = document.getElementById("burger-menu");
 var overlay = document.getElementById("menu");
 let tl = gsap.timeline();
 
+let menu = document.getElementsByClassName("menu_item");
+
+for (let i = 0; i < menu.length; i++) {
+  const el = menu[i];
+  el.addEventListener("click", function () {
+    overlay.classList.toggle("overlay");
+  });
+}
+
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
