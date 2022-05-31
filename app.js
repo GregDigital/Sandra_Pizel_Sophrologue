e5fe23ca091af04e481fe800bc9b7ea86399c281
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-05-31 14:06:09 +0200

    app

--- app.js
+++ app.js
@@ -11,38 +11,7 @@ burgerMenu.addEventListener("click", function () {
 
 //
 
-let menu = document.getElementsByClassName("menu_item");
-
-for (let i = 0; i < menu.length; i++) {
-  const el = menu[i];
-  el.addEventListener("click", function () {
-    overlay.classList.toggle("overlay");
-    burgerMenu.classList.toggle("active");
-    body.classList.toggle("true");
-
-    tl.from("li", {
-      opacity: 1,
-    });
-  });
-}
-
 // hide logo while scrolling
-
-let lastScroll = 20;
-let logo = document.querySelector(".hideLogo");
-let h2 = document.querySelector(".hideMenu");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY < lastScroll) {
-    logo.style.visibility = "visible";
-    h2.style.visibility = "visible";
-  } else {
-    logo.style.visibility = "hidden";
-    h2.style.visibility = "hidden";
-  }
-  // transmet la position quand le scroll est terminé
-  return lastScroll;
-});
 
 // Custom cursor
 
