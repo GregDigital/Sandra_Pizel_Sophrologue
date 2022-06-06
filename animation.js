3449acf5fc4efa37b7fe07a1fd9fed4e5a526d20
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-06-06 15:51:37 +0200

    animation v2

--- animation.js
+++ animation.js
@@ -2,19 +2,68 @@ let animation;
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.set("#frame_bottom", { transformOrigin: "50% 50%" });
+gsap.set("#green", { xPercent: -50 });
 
-animation = gsap.to("#frame_bottom", {
-  scrollTrigger: {
-    start: "top 5%",
-    end: "+=7000px",
-    scrub: 1,
-    markers: false,
-    onUpdate: (self) => {
-      gsap.to("#frame_bottom", {
-        rotation: () => (self.direction === 1 ? 0 : -180),
-        overwrite: "auto",
-      });
+var rotate = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#frame_bottom",
+      scrub: 0.2,
+      start: "top 5%",
+      end: "+=8000",
     },
-  },
-});
+  })
+  .to("#frame_bottom", {
+    rotation: 360 * 5,
+    duration: 1,
+    ease: "none",
+  });
+gsap.set("#frame_right", { right: 50 });
+
+var rotate = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#frame_right",
+      scrub: 0.2,
+      start: "top 5%",
+      end: "+=8000",
+    },
+  })
+  .to("#frame_right", {
+    rotation: 360 * 5,
+    duration: 1,
+    ease: "none",
+  });
+gsap.set("#frame_left", { left: 50 });
+
+var rotate = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#frame_left",
+      scrub: 0.2,
+      start: "top 5%",
+      end: "+=8000",
+    },
+  })
+  .to("#frame_left", {
+    rotation: 360 * 5,
+    duration: 2,
+    ease: "none",
+  });
+
+gsap.set("#frame_top", { top: 50, right: 350 });
+
+var rotate = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#frame_top",
+      scrub: 1,
+      start: "top top",
+      end: "+7000",
+    },
+  })
+  .to("#frame_top", {
+    rotation: 360 * 4,
+    duration: 1,
+    ease: "none",
+  });
